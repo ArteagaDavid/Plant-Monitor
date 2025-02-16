5f69c602e4cb7e729f1b0d3cf94cd109de87fc43
David Arteaga <dvdrtg26@gmail.com>
2025-02-16 16:38:03 -0700

    Some documentation/refactoring and Enabled the Database.

--- Central_Server/mqtt_server.py
+++ Central_Server/mqtt_server.py
@@ -7,27 +7,29 @@ import json
 from datetime import datetime
 
 from ipywidgets import Controller
+# from plant_controller import PlantController
 
+from db_handler import DataBaseHandler
 
-# from database_handler import DatabaseHandler
 # from ml_predictor import MLPredictor
 class MQTTServer:
     def __init__(self):
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
-
-        # self.db = DatabaseHandler()
+# Here we initialize the modules needed. Database, Plant controll for the automation, and ML for future inference implementation
+        self.db = DataBaseHandler()
         # self.controller = PlantController(self.db)
         # self.ml_predictor = MLPredictor()
 
-        # Topics
+        # Topics are what we subscribe to. (Central server Pi) sensors for data collection. Control watering and light control
         self.SENSOR_TOPIC = "garden/+/sensors"  # + is wildcard for plant_id
         self.CONTROL_TOPIC = "garden/{}/control"
 
 
     def start(self):
         try:
+            # This port is the Pi's server port.
             self.client.connect("localhost", 1883, 60)
             #logic in case of disconnect try to reconnect
             self.client.on_disconnect = self.on_disconnect
@@ -35,7 +37,7 @@ class MQTTServer:
         except Exception as e:
             print(f"Failed to start MQTT server {e}")
 
-    def on_disconnect(self, client, userdata, rc):
+    def on_disconnect(self, client, rc):
         print(f"Disconnected with result code {rc}")
         if rc !=0:
             try:
@@ -43,7 +45,7 @@ class MQTTServer:
             except Exception as e:
                 print(f"Failed to reconnect MQTT server {e}")
 
-    def on_connect(self, client, userdata, flags, rc):
+    def on_connect(self, client, flags, rc):
         print("Connected with result code "+str(rc))
         # We only do sensor since we do not need control logic at this time
         client.subscribe(self.SENSOR_TOPIC)
@@ -52,9 +54,16 @@ class MQTTServer:
         required_fields = ["temperature", "humidity", "light"]
         return all(field in payload for field in required_fields)
 
-    def on_message(self, client, userdata, msg):
+    def on_message(self, client, msg):
+        """
+        Collects data from sensors stores into the db, Then gets automation/control instructions
+        and publishes it to MQTT server
+        :param client: MQTT client
+        :param msg: incoming message from Arduino(Plant)
+        :return: Returns the automation/control instructions
+        """
         try:
-            #Extract plant_id from topic
+            #Extract plant_id from topic. Plant id identifies the plant or edge Arduino node.
             plant_id = msg.topic.split("/")[1]
             payload = json.loads(msg.payload.decode())
             if not self.validate_sensor_data(payload):
@@ -68,7 +77,8 @@ class MQTTServer:
             # Get ML predictions
             # ml_predictions = self.ml_predictor.predict(plant_id,payload)
 
-            #Where we calculte the automation decisions for garden
+            #Where we calculte the automation decisions for garden.
+            # Future we will introduce ML model to start controlling
             automation_decisions = self.controller.get_control_decisions(
                 sensor_data = payload,
                 settings = settings
